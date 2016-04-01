c18647aaa19f801b33ed9a4369a1eac6e720543d
kidd3166 <hellokiki3166@163.com>
2016-04-01 18:00:13 +0800

    test

---
+++ library/src/main/java/com/ouj/library/adapter/RefreshAdapter.java
@@ -0,0 +1,42 @@
+package com.ouj.library.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+import com.ouj.library.helper.RefreshPtrHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liqi on 2016-4-1.
+ */
+public abstract class RefreshAdapter<E, VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> implements RefreshPtrHelper.DataStore {
+
+    public final ArrayList<E> items = new ArrayList<>();
+
+    @Override
+    public int getItemCount() {
+        return items == null ? 0 : items.size();
+    }
+
+    @Override
+    public void setItems(List list, boolean b) {
+        if (items == null)
+            return;
+        if (b)
+            items.clear();
+        items.addAll(list);
+    }
+
+    @Override
+    public void clear() {
+        if (items == null)
+            return;
+        items.clear();
+    }
+
+    @Override
+    public int getCount() {
+        return items == null ? 0 : items.size();
+    }
+}
