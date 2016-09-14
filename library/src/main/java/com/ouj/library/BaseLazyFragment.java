e0b83290b624d01add4eaa7ae8669f1e58c2e7ff
kidd3166 <hellokiki3166@163.com>
2016-09-14 10:36:18 +0800

    test

--- library/src/main/java/com/ouj/library/BaseLazyFragment.java
+++ library/src/main/java/com/ouj/library/BaseLazyFragment.java
@@ -1,19 +1,30 @@
 package com.ouj.library;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
 /**
  * Created by liqi on 2016-4-8.
  */
 public abstract class BaseLazyFragment extends BaseFragment {
 
     private boolean isLazyLoad;
+    private boolean isViewCreated;
 
     protected abstract void onLazyLoad();
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        isViewCreated = true;
+    }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
         if (isVisibleToUser) {
-            if (!isLazyLoad) {
+            if (isViewCreated && !isLazyLoad) {
                 isLazyLoad = true;
                 onLazyLoad();
             }
